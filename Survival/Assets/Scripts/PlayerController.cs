543f4bd40c2325b5711da2c3776e80beca22808b
LowLie <dfernandesbatista@gmail.com>
2025-03-10 15:06:18 +0100

    Ajust angle camara i moviment personatge

--- Survival/Assets/Scripts/PlayerController.cs
+++ Survival/Assets/Scripts/PlayerController.cs
@@ -20,10 +20,12 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        // Obtenemos los inputs de Horizontal y Vertical
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
 
-        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        // Nueva dirección de movimiento que mezcla ambos ejes
+        Vector3 movementDirection = new Vector3(h + v, 0, v - h);
         float magnitude = Mathf.Clamp01(movementDirection.magnitude);
 
         // Si Shift está presionado, duplica la velocidad
@@ -31,34 +33,17 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftShift))
         {
             animator.SetBool("EstaCorrent", true);
-
             currentSpeed *= 2;
         }
         else
         {
             animator.SetBool("EstaCorrent", false);
-
         }
 
         movementDirection.Normalize();
         float finalSpeed = magnitude * currentSpeed;
 
         ySpeed += Physics.gravity.y * Time.deltaTime;
-
-        //if (characterController.isGrounded)
-        //{
-        //    characterController.stepOffset = originalStepOffset;
-        //    ySpeed = -0.5f;
-
-        //    if (Input.GetButtonDown("Jump"))
-        //    {
-        //        ySpeed = jumpSpeed;
-        //    }
-        //}
-        //else
-        //{
-        //    characterController.stepOffset = 0;
-        //}
 
         Vector3 velocity = movementDirection * finalSpeed;
         velocity.y = ySpeed;
