908d33f977b3a1e1e8af8cf890f4b0ae6cb88b40
Aleix Puigmal <apuigmal@uda.ad>
2025-04-15 09:40:19 +0200

    Update spawn points

--- Survival/Assets/Scripts/Constants/TPConstants.cs
+++ Survival/Assets/Scripts/Constants/TPConstants.cs
@@ -5,25 +5,25 @@ public static class TPConstants
 {
     // Pedrosa (Aleix)
     public static readonly string PEDROSA_SCENE = "Aleix";
-    public static readonly Vector3 PEDROSA_SPAWN_POINT = new Vector3(120.52f, 52.4f, -105.5f);
+    public static readonly Vector3 PEDROSA_SPAWN_POINT = new Vector3(136.06f, 42f, -97.25f);
     
     // Hub (David)
     public static readonly string HUB_SCENE = "David";
-    public static readonly Vector3 HUB_SPAWN_POINT = new Vector3(-71.5f, 50.97f, -189.5f);
+    public static readonly Vector3 HUB_SPAWN_POINT = new Vector3(-75.7f, 66.6f, 87.7f);
     
     // Tutorial
     public static readonly string TUTORIAL_SCENE = "David"; 
-    public static readonly Vector3 TUTORIAL_SPAWN_POINT = new Vector3(-71.5f, 50.97f, -189.5f);
+    public static readonly Vector3 TUTORIAL_SPAWN_POINT = new Vector3(-185.03f, 21.79f, -534.41f);;
     
     // Bosc (Jano)
     public static readonly string BOSC_SCENE = "Jano";
-    public static readonly Vector3 BOSC_SPAWN_POINT = new Vector3(0, 0, 0);
+    public static readonly Vector3 BOSC_SPAWN_POINT = new Vector3(58.04f, 6.11f, 7.42f);
     
     // Engo (Alejandro)
     public static readonly string ENGO_SCENE = "Alejandro";
-    public static readonly Vector3 ENGO_SPAWN_POINT = new Vector3(0, 0, 0);
+    public static readonly Vector3 ENGO_SPAWN_POINT = new Vector3(12.6f, 3.2f, -26.4f);
     
     // Ramio (Wadoud)
     public static readonly string RAMIO_SCENE = "Wadoud";
-    public static readonly Vector3 RAMIO_SPAWN_POINT = new Vector3(0, 0, 0);
+    public static readonly Vector3 RAMIO_SPAWN_POINT = new Vector3(-157.06f, 4.86f, -204.1f);
 }
