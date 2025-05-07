ade67c5a956179c6ee6ca6f77444e786200518ef
erkgd <alejandrogarrancho@gmail.com>
2025-05-07 11:59:40 +0200

    Fix indentation in video playback logic in SistemaVideo

--- Survival/Assets/Scripts/Sistemes/SistemaVideo.cs
+++ Survival/Assets/Scripts/Sistemes/SistemaVideo.cs
@@ -57,7 +57,7 @@ public class SistemaVideo : MonoBehaviour
             CargarVideo(rutaVideo);
             
             if (reproducirAutomaticamente)
-            {º
+            {
                 videoPlayer.Play();
             }
         }
